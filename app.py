0784cbf3fb8ff7076b7943d0667d5027101b7de1
fly <98468939+fly1014gk@users.noreply.github.com>
2025-06-03 14:40:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import time
 import os
 
 app = Flask(__name__)
-SIZE = 100  # キャンバスサイズを100*100に
-canvas = [[0 for _ in range(SIZE)] for _ in range(SIZE)]
+SIZE = 100
+DEFAULT_COLORS = ["#fff", "#f00", "#00f", "#0f0", "#000"]
+canvas = [[DEFAULT_COLORS[0] for _ in range(SIZE)] for _ in range(SIZE)]
 updates = []
 
 @app.route('/')
 def index():
-    return render_template('index.html', canvas=canvas, size=SIZE)
+    return render_template('index.html', canvas=canvas, size=SIZE, default_colors=DEFAULT_COLORS)
 
 @app.route('/canvas')
 def get_canvas():
